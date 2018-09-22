55d545fdf103c8e85d3656744c28edb41491c330
zhangjhboy <43024890+zhangjhboy@users.noreply.github.com>
2018-09-22 10:05:30 +0800

    Update Audi.java

--- Audi.java
+++ Audi.java
@@ -4,26 +4,26 @@ public class Audi
         public static void main(String[] args)
         {
                 int i = 0;
-	              int num = 10;//车辆数量
-	              Scanner input = new Scanner(System.in);
-	              Audi[] array = new Audi[num];
-	              //输入车辆信息
-	              for (i = 0; i < num; i++)
-	              {
-	                      array[i] = new Audi();
+	        int num = 10;//车辆数量
+	        Scanner input = new Scanner(System.in);
+	        Audi[] array = new Audi[num];
+	        //输入车辆信息
+	        for (i = 0; i < num; i++)
+	        {
+	                array[i] = new Audi();
                         System.out.println("请输入第" + (i + 1) + "辆汽车的信息：");
-	                      System.out.println("汽车型号：");
-	                      array[i].model = input.next();
-	                      System.out.println("当前油量：");
-	                      array[i].tank = input.nextDouble();
-	                      System.out.println("油耗：");
-	                      array[i].oilConsumption= input.nextDouble();
-	              }
-	               //输出车辆信息
-	              for (i = 0; i < num; i++)
+	                System.out.println("汽车型号：");
+	                array[i].model = input.next();
+	                System.out.println("当前油量：");
+	                array[i].tank = input.nextDouble();
+	                System.out.println("油耗：");
+	                array[i].oilConsumption= input.nextDouble();
+	        }
+	          //输出车辆信息
+	        for (i = 0; i < num; i++)
                 {
-	                      System.out.println("第" + (i + 1) + "辆车的信息");
-	                      System.out.println("汽车型号：" + array[i].model + "  " + "当前油量：" + array[i].tank + "  " + "油耗：" + array[i].oilConsumption);
+	                System.out.println("第" + (i + 1) + "辆车的信息");
+	                System.out.println("汽车型号：" + array[i].model + "  " + "当前油量：" + array[i].tank + "  " + "油耗：" + array[i].oilConsumption);
                 }
         }
         public String model;//汽车型号
